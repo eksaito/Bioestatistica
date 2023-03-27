bd3aee5586f2beb4507e9e00dc3e2978e8e3e66b
eksaito <erickauati@gmail.com>
2023-03-27 15:11:28 -0300

    Update pratica_testes.R

--- pratica_testes.R
+++ pratica_testes.R
@@ -1,47 +1,47 @@
 ## Trabalho 
 #
-# Script de exemplo para o trabalho final de bioestatÌstica 2022.2
-# - Testes estatÌsticos
+# Script de exemplo para o trabalho final de bioestat√≠stica 2022.2
+# - Testes estat√≠sticos
 #
 # Eric Kauati Saito
 # 31/10/2022
 
 # Carregar dados do arquivo
 
-dados <- read.table(file="D:\\Macae Disciplinas\\2022\\R\\healthcare-dataset-stroke-data.csv", sep=",", header=T)
+dados <- read.table(file="healthcare-dataset-stroke-data.csv", sep=",", header=T)
 
 # Trabalhando com os dados
-## Primeiras an·lises
+## Primeiras an√°lises
 dados_reduzidos = dados[1:400,]
 
-## Testes estatÌsticos
+## Testes estat√≠sticos
 
-### Intervalo de confianÁa
+### Intervalo de confian√ßa
 n_hipertenso = dados_reduzidos[dados_reduzidos$hypertension==0,]
 hipertenso = dados_reduzidos[dados_reduzidos$hypertension==1,]
 
-t.test(hipertenso$avg_glucose_level, conf.level = 0.9) #Intervalo de confianÁa para 90%
+t.test(hipertenso$avg_glucose_level, conf.level = 0.9) #Intervalo de confian√ßa para 90%
 
 ### teste t
 t.test(n_hipertenso$avg_glucose_level, hipertenso$avg_glucose_level, alternative = "two.sided") #less, greater
-#ou ent„o, podiamos ter feito sem precisar separar as colunas
+#ou ent√£o, podiamos ter feito sem precisar separar as colunas
 #t.test(dados_reduzidos$avg_glucose_level ~ dados_reduzidos$hypertension, alternative = "two.sided")
 
 boxplot(dados_reduzidos$avg_glucose_level ~ dados_reduzidos$hypertension,
-        main = "NÌvel de glicose e Hipertens„o",
-        ylab = "NÌvel de glicose (mg/dL)",
-        xlab= "Hipertens„o",
-        names = c('N„o', 'Sim'),
+        main = "N√≠vel de glicose e Hipertens√£o",
+        ylab = "N√≠vel de glicose (mg/dL)",
+        xlab= "Hipertens√£o",
+        names = c('N√£o', 'Sim'),
         col = c('orange','blue'))
 
 ### teste t pareado
-# N„o vai funcionar porque o tamanho das amostras s„o diferentes (e n„o s„o dados pareados)
+# N√£o vai funcionar porque o tamanho das amostras s√£o diferentes (e n√£o s√£o dados pareados)
 # t.test(hipertenso$avg_glucose_level, n_hipertenso$avg_glucose_level, alternative = "two.sided", paired = TRUE)
 
 
 ### Wilcoxon rank sum test
-# Testes n„o-paramÈtrico para duas amostras INDEPENDENTES
-# Wilcoxon signed-rank test -> n„o paramÈtrico, pareado
+# Testes n√£o-param√©trico para duas amostras INDEPENDENTES
+# Wilcoxon signed-rank test -> n√£o param√©trico, pareado
 wilcox.test(hipertenso$avg_glucose_level, n_hipertenso$avg_glucose_level, alternative = "two.sided")
 
 ### ANOVA
@@ -51,7 +51,7 @@ oneway.test(age ~ smoking_status, data=dados_reduzidos, var.equal=T)
 model <- aov(age ~ smoking_status, data=dados_reduzidos)
 TukeyHSD(model, conf.level=.95)
 
-### Teste para 2 proporÁıes
+### Teste para 2 propor√ß√µes
 tabela = table(dados_reduzidos$hypertension, dados_reduzidos$Residence_type)
 n_hipertensos = c(41,42)
 residencia = c(194,206) #41+153, 42+164
@@ -59,5 +59,5 @@ residencia = c(194,206) #41+153, 42+164
 prop.test(n_hipertensos, residencia, alternative = "two.sided",correct = FALSE)
 
 barplot(tabela, beside = TRUE,
-        legend = c("Press„o normal","Press„o alta"),
+        legend = c("Press√£o normal","Press√£o alta"),
         ylim=c(0,200))
